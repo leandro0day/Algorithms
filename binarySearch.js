e76e3374921f89b6829d84667cefa4d32dfa2377
Leandro Matus <leandromatus85@gmail.com>
2024-01-16 12:16:35 -0600

    Implemented the binary search algorithm in JavaScript. - The `binarySearch` function efficiently finds an element in a sorted array, returning its index or `null` if not found.

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+function binarySearch(arr, item) {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = arr[mid];
+
+    if (guess == item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
+}
+
+let myList = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(myList, 3));  // => 1
+console.log(binarySearch(myList, -1)); // => null
